e1449a78ac532114571a00f0faed72cfc7dbdd26
106360130 <bs25017@gmail.com>
2019-11-14 23:41:56 +0800

    first

---
+++ app/src/main/java/com/example/lab5kotlinpart/FirstFragment.kt
@@ -0,0 +1,62 @@
+package com.example.lab5kotlinpart
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import android.view.LayoutInflater
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("FristFragment", "onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        Log.e("FristFragment", "onCreate")
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.e("FirstFragment", "onActivityCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("FirstFragment", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("FirstFragment", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("FirstFragment", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("FirstFragment", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.e("FirstFragment", "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("FirstFragment", "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.e("FirstFragment", "onDetach")
+    }
+}
---
+++ app/src/main/java/com/example/lab5kotlinpart/SecondFragment.kt
@@ -0,0 +1,62 @@
+package com.example.lab5kotlinpart
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import android.view.LayoutInflater
+
+class SecondFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("SecondFragment", "onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        Log.e("SecondFragment", "onCreateView")
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.e("SecondFragment", "onActivityCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("SecondFragment", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("SecondFragment", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("SecondFragment", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("SecondFragment", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.e("SecondFragment", "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("SecondFragment", "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.e("SecondFragment", "onDetach")
+    }
+
+}
